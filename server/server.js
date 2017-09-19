7735a7c1d9eab57f606c762616c082725b0f8893
mohamedElsonny <mohamedelsonny5@gmail.com>
2017-09-20 00:36:14 +0200

    Great new user and other

--- server/server.js
+++ server/server.js
@@ -13,13 +13,34 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New User in',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (msg) => {
         console.log('new message recived: ', msg);
+
+
         io.emit('newMessage', {
             from: msg.from,
             text: msg.text,
             createdAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: msg.from,
+        //     text: msg.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
